1ff526bc6c595cb5a58b8555fde702d773e362a1
Szymon Wolny <sim32@op.pl>
2018-04-08 22:42:59 +0200

    odc-4

---
+++ odc-4/materialy.py
@@ -0,0 +1,25 @@
+#KONWERSJA LICZB
+#############################
+#int(3.8)     #3
+#float(4)     #4.0
+
+# liczba_float = 3.15
+# konwersja_na_int = int(liczba_float)
+# print(konwersja_na_int)     #3
+
+# OPERACJE ARYTMETYCZNE
+##############################
+#     Dodawanie       
+# print(3+3)          #6
+#     Odejmowanie     
+# print(3-1)          #2
+#     Mnożenie        
+# print(2*2)          #4
+#     Dzielenie       
+# print(2/2)          #1.0
+#     Potęgowanie     
+# print(2**2)         #4
+#     Modulo          
+# print(5%3)          #2
+#     Floor division  
+# print(5//2)         #2
