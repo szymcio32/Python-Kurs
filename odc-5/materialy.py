301579482373d9f776b2bf6309e99d0743ab0c95
Szymon Wolny <sim32@op.pl>
2018-04-19 20:11:30 +0200

    odc-5

---
+++ odc-5/materialy.py
@@ -0,0 +1,29 @@
+
+#Zmienne przechowujace tekst to zmienne typu string
+
+##TWORZENIE ZMIENNEJ TYPU STRING
+
+# wiadomosc1 = "Mam na imie Szymon"
+#
+# wiadomosc2 = 'Mam na imie Jan'
+#
+# wiadomosc3 = "He's name is Szymon"
+#
+# wiadomosc4 = 'Mam na "imie" Szymon'
+#
+# wiadomosc5 = """He's name is Szymon
+# Mam na imie Szymon
+# Ala ma kota"""
+#
+# wiadomosc6 = '''He's name is Szymon
+# Mam na imie Szymon
+# Ala ma kota'''
+
+
+##PRINTOWANIE NA KONSOLE
+# print(wiadomosc1)
+# print(wiadomosc2)
+# print(wiadomosc3)
+# print(wiadomosc4)
+# print(wiadomosc5)
+# print(wiadomosc6)
