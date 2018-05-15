c006e3a99499db71a494b9d90f823d076f43259b
Szymon Wolny <sim32@op.pl>
2018-05-15 21:35:57 +0200

    odc-7

---
+++ odc-7/materialy.py
@@ -0,0 +1,24 @@
+##KONKATENACJA
+#Konkatenacja oznacza łączenie dwóch wyrażeń (np. tekstowych) w jedno
+
+# imie = 'Jan'
+# nazwisko = 'Adamczyk'
+# osoba_1 = imie + ' ' + nazwisko
+# print(osoba_1)        # Jan Adamczyk
+
+# osoba_2 = 'Karol' + ' ' + 'Urban'
+# print(osoba_2)        # Karol Urban
+
+# wiek = '24' + '2'
+# wiek = '24' + str(2)
+# print(wiek)           # 242
+
+##POWTARZANIE
+
+# imie = 'Ewelina'
+# print(imie * 3)       # EwelinaEwelinaEwelina
+
+
+# ksiazka_1 = 'Zbrodnia i kara'
+# ksiazka_2 = 'Przygody Alicji w Krainie Czarow'
+# print(ksiazka_1[:9] + ksiazka_2[9:])      # Zbrodnia Alicji w Krainie Czarow
