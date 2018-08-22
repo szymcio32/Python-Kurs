e60240e1a31a7ca66272f6a1a4e1124d0b45695d
szymon <sim32@op.pl>
2018-08-22 22:41:47 +0200

    odc-14

---
+++ odc-14/materialy.py
@@ -0,0 +1,63 @@
+nieznajomy = "ork"
+
+if nieznajomy == 'ork':            # True
+    print('Atakuj!')               # Atakuj!
+
+
+# nieznajomy = "człowiek"
+#
+# if nieznajomy == "ork":           # False
+#     print('Atakuj!')
+# else:
+#     print('To nic groźnego')      # To nic groźnego
+
+
+# nieznajomy = "człowiek"
+#
+# if nieznajomy == 'ork':           # False
+#     print('Atakuj!')
+# elif nieznajomy == 'człowiek':    # True
+#     print('To nasz sojusznik')    # To nasz sojusznik
+# else:
+#     print('To nic groźnego')
+
+
+### OPERATORY PORÓWNANIA
+# ==    równe
+# !=    nierówne
+# >     większe
+# >=    większe lub równe
+# <     mniejsze
+# <=    mniejsze lub równe
+
+
+# liczba = 1
+#
+# if liczba > 0:                                            # True
+#     print('Liczba {} jest większa od 0'.format(liczba))   # Liczba 1 jest większa od 0
+# else:
+#     print('Liczba {} jest miejsza od 0'.format(liczba))
+#
+
+
+# lista_bohaterow = ['Arthas, Illidan', 'Thrall']
+#
+# if 'Thrall' in lista_bohaterow:           # True
+#     print('Tak, to nasz bohater')         # Tak, to nasz bohater
+# else:
+#     print('Nie... on nam nie pomoże')
+
+
+### OBIEKTY TYPU FALSE W PYTHONIE
+# None
+# False
+# Zero - kazdy numeryczny typ
+# Puste elementy np. '' [] () {}
+
+
+# warunek = "Python"
+#
+# if warunek:                               # True
+#     print('Warunek prawdziwy')            # Warunek prawdziwy
+# else:
+#     print('Warunek fałszywy')
